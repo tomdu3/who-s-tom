486d3e2ee4fafba17907d0a0d053923f63926824
Tomislav Dukez <tomdu3@ymail.com>
2025-08-10 17:51:35 +0100

    add js code for hamburger menu

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -3,3 +3,14 @@ const h2 = document.querySelector('h2');
 setTimeout(() => {
     h2.textContent = "Tom's a superstar";
 }, 2000);
+
+
+const showSidebar = () => {
+    const sidebar = document.querySelector('.sidebar');
+    sidebar.style.display = 'flex';
+}
+
+const hideSidebar = () => {
+    const sidebar = document.querySelector('.sidebar');
+    sidebar.style.display = 'none';
+}
